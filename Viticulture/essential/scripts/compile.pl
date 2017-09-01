379cdf73f5cb2d0e98a7236a25bddfa7b5c91ae9
wil_low <wil_low@localhost>
2017-09-02 02:23:27 +0300

    Scripts added

---
+++ Viticulture/essential/scripts/compile.pl
@@ -0,0 +1,18 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my @export = glob ('../export/visitors-page*.png');
+=head
+foreach my $img (@export) {
+	$img =~ /(visitors-page\d+\.png)/;
+	my $out = "../img-crosses/$1";
+	`gm composite ../signs/crosses.png $img $out`;
+	print "$out\n";
+}
+=cut
+
+my $cmd = "gm montage \@cards.lst +adjoin -tile 4x4 -geometry +0+0 ../4x4/%02d.png";
+print "$cmd\n";
+system ($cmd);
